61cc3e74f424637d41e06e960ab2703eabf0931d
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2020-09-15 13:40:58 +0100

    Add proper encoding support.

--- admin/controllers/csvupload.php
+++ admin/controllers/csvupload.php
@@ -274,7 +274,7 @@ class CSVUploadsControllerCSVUpload extends JControllerForm
      * Converts CSV to array.
      *
      * @param   string   $csv          CSV string, probably sent from a file.
-     * @param   bool     $header_keys  Does the CSV contain an intial row of column names?
+     * @param   bool     $header_keys  Does the CSV contain an initial row of column names?
      */
     public function csvarray($csv, $header_keys = false)
     {
@@ -283,6 +283,7 @@ class CSVUploadsControllerCSVUpload extends JControllerForm
             return false;
         }
 
+        $csv = $this->w1250_to_utf8($csv);
         $csv = preg_replace('/(\r|\n\r|\r\n)/', '\n', $csv);
 
         // Remove breaks from within quotes:
@@ -294,7 +295,6 @@ class CSVUploadsControllerCSVUpload extends JControllerForm
             }
         }
 
-        $csv        = utf8_encode($csv);
         $csv_array  = explode("\n", $csv);
         $data       = array();
         $cell_total = 0;
@@ -359,6 +359,64 @@ class CSVUploadsControllerCSVUpload extends JControllerForm
             unset($data[0]);
         }
         return $data;
+    }
+
+    /**
+     * Windows encoding to utf8.
+     *
+     * @param   string  $csv    CSV data.
+     *
+     * Taken from: https://www.php.net/manual/en/function.mb-convert-encoding.php
+    */
+    public function w1250_to_utf8($csv) {
+        // map based on:
+        // http://konfiguracja.c0.pl/iso02vscp1250en.html
+        // http://konfiguracja.c0.pl/webpl/index_en.html#examp
+        // http://www.htmlentities.com/html/entities/
+        $map = array(
+            chr(0x8A) => chr(0xA9),
+            chr(0x8C) => chr(0xA6),
+            chr(0x8D) => chr(0xAB),
+            chr(0x8E) => chr(0xAE),
+            chr(0x8F) => chr(0xAC),
+            chr(0x9C) => chr(0xB6),
+            chr(0x9D) => chr(0xBB),
+            chr(0xA1) => chr(0xB7),
+            chr(0xA5) => chr(0xA1),
+            chr(0xBC) => chr(0xA5),
+            chr(0x9F) => chr(0xBC),
+            chr(0xB9) => chr(0xB1),
+            chr(0x9A) => chr(0xB9),
+            chr(0xBE) => chr(0xB5),
+            chr(0x9E) => chr(0xBE),
+            chr(0x80) => '&euro;',
+            chr(0x82) => '&sbquo;',
+            chr(0x84) => '&bdquo;',
+            chr(0x85) => '&hellip;',
+            chr(0x86) => '&dagger;',
+            chr(0x87) => '&Dagger;',
+            chr(0x89) => '&permil;',
+            chr(0x8B) => '&lsaquo;',
+            chr(0x91) => '&lsquo;',
+            chr(0x92) => '&rsquo;',
+            chr(0x93) => '&ldquo;',
+            chr(0x94) => '&rdquo;',
+            chr(0x95) => '&bull;',
+            chr(0x96) => '&ndash;',
+            chr(0x97) => '&mdash;',
+            chr(0x99) => '&trade;',
+            chr(0x9B) => '&rsquo;',
+            chr(0xA6) => '&brvbar;',
+            chr(0xA9) => '&copy;',
+            chr(0xAB) => '&laquo;',
+            chr(0xAE) => '&reg;',
+            chr(0xB1) => '&plusmn;',
+            chr(0xB5) => '&micro;',
+            chr(0xB6) => '&para;',
+            chr(0xB7) => '&middot;',
+            chr(0xBB) => '&raquo;',
+        );
+        return html_entity_decode(mb_convert_encoding(strtr($csv, $map), 'UTF-8', 'ISO-8859-2'), ENT_QUOTES, 'UTF-8');
     }
 
     /**
